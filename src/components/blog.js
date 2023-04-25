dc8e1edf1359d1319be05dbc8e0be9b3a0b6e76a
Luan Tavares <luankriok18@gmail.com>
2023-04-25 08:26:57 -0300

    first commit

---
+++ src/components/blog.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./blog.css";
+
+export default function Blog({ posts }) {
+  return (
+    <div className="postsContainer">
+      {/* EM UMA SITUAÇÃO REAL, EU USARIA O MAP*/}
+      {/* {posts.map((post) => (
+        <div className="postsBox">
+          <h1>{post.titulo}</h1>
+          <p>{post.texto}</p>
+        </div>
+      ))} */}
+
+      {/* MAS VOU USAR DO MODO MAIS MANUAL, CONFORME COMBINADO. */}
+      <div className="postsBox">
+        <h1>{posts[0].titulo}</h1>
+        <p>{posts[0].texto}</p>
+      </div>
+      <div className="postsBox">
+        <h1>{posts[1].titulo}</h1>
+        <p>{posts[1].texto}</p>
+      </div>
+      <div className="postsBox">
+        <h1>{posts[2].titulo}</h1>
+        <p>{posts[2].texto}</p>
+      </div>
+      <div className="postsBox">
+        <h1>{posts[3].titulo}</h1>
+        <p>{posts[3].texto}</p>
+      </div>
+    </div>
+  );
+}
