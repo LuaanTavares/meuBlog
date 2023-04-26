6c3fdd75da7212d98960dbed1fe63796b02fed18
Luan Tavares <luankriok18@gmail.com>
2023-04-26 10:05:50 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,71 @@
 import "./App.css";
 import React from "react";
 import Blog from "./components/blog";
+import { useState } from "react";
 
 function App() {
-
-  // EM UMA SITUAÇÃO REAL, EU CRIARIA 2 USESTATEs (UM PARA O TITULO E OUTRO PARA O TEXTO),
+  // EM UMA SITUAÇÃO REAL, EU CRIARIA UM USESTATE ]
   // E PEDIRIA ESSA ENTRADA VIA INPUT, PARA SETAR O VALOR DO ARRAY.
-  const postsArray = [
-    {
-      titulo: "Primeira Postagem",
-      texto: "Esta é a primeira postagem: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      titulo: "Segunda Postagem",
-      texto: "Esta é a segunda postagem: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    },
-    {
-      titulo: "Terceira Postagem",
-      texto: "Esta é a terceira postagem: On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains",
-    },
-    {
-      titulo: "Quarta Postagem",
-      texto: "Esta é a quarta postagem: Existem muitas variações disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do texto. Todos os geradores de Lorem Ipsum na internet tendem a repetir pedaços predefinidos conforme necessário, fazendo deste o primeiro gerador de Lorem Ipsum autêntico da internet. Ele usa um dicionário com mais de 200 palavras em Latim combinado com um punhado de modelos de estrutura de frases para gerar um Lorem Ipsum com aparência razoável, livre de repetições, inserções de humor, palavras não características, etc.",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  console.log(posts);
+
+  const savePost = () => {
+    const titulo = document.getElementById("titulo").value;
+    const texto = document.getElementById("texto").value;
+
+    const data = {
+      titulo,
+      texto,
+    };
+
+    titulo.length && texto.length > 4 ? setPosts((e) => [...e, data]) : window.alert("Digite uma postagem válida")
+  };
+
+  // ESTA SERIA UMA FORMA MAIS MANUAL
+  // const postsArray = [
+  //   {
+  //     titulo: "Primeira Postagem",
+  //     texto:
+  //       "Esta é a primeira postagem: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   },
+  //   {
+  //     titulo: "Segunda Postagem",
+  //     texto:
+  //       "Esta é a segunda postagem: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+  //   },
+  //   {
+  //     titulo: "Terceira Postagem",
+  //     texto:
+  //       "Esta é a terceira postagem: On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains",
+  //   },
+  //   {
+  //     titulo: "Quarta Postagem",
+  //     texto:
+  //       "Esta é a quarta postagem: Existem muitas variações disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito escondido no meio do textoo. Todos os geradores de Lorem Ipsum na internet tendem a repetir pedaços predefinidos conforme necessário, fazendo deste o primeiro gerador de Lorem Ipsum autêntico da internet. Ele usa um dicionário com mais de 200 palavras em Latim combinado com um punhado de modelos de estrutura de frases para gerar um Lorem Ipsum com aparência razoável, livre de repetições, inserções de humor, palavras não características, etc.",
+  //   },
+  // ];
 
   return (
     <div className="app">
       <h1 className="blogTitulo">Meu Blog</h1>
+
+      <form className="formulario">
+        <input placeholder="Digite seu Título" className="tituloInput" type="texto" id="titulo" />
+        <textarea placeholder="Digite seu Texto" className="textoInput" type="texto" id="texto" />
+
+        <button onClick={savePost} type="button">
+          Postar
+        </button>
+      </form>
+
       <div className="postsContainer">
-        <Blog posts={postsArray} />
+        <Blog posts={posts} />
       </div>
+
+      
+      {/* <div className="postsContainer">
+        <Blog posts={postsArray} />
+      </div> */}
     </div>
   );
 }
--- src/components/blog.js
+++ src/components/blog.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./blog.css";
 
-export default function Blog({ posts }) {
+export default function Blog({posts}) {
+  console.log(posts)
   return (
     <div className="postsContainer">
       {/* EM UMA SITUAÇÃO REAL, EU USARIA O MAP*/}
-      {/* {posts.map((post) => (
+      {posts.map((post) => (
         <div className="postsBox">
-          <h1>{post.titulo}</h1>
-          <p>{post.texto}</p>
+          <h1 >{post.titulo}</h1>
+          <p >{post.texto}</p>
         </div>
-      ))} */}
+      ))}
 
-      {/* MAS VOU USAR DO MODO MAIS MANUAL, CONFORME COMBINADO. */}
-      <div className="postsBox">
+      {/* ESTA SERIA UMA FORMA MAIS MANUAL */}
+      {/* <div className="postsBox">
         <h1>{posts[0].titulo}</h1>
         <p>{posts[0].texto}</p>
       </div>
@@ -28,7 +29,7 @@ export default function Blog({ posts }) {
       <div className="postsBox">
         <h1>{posts[3].titulo}</h1>
         <p>{posts[3].texto}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
